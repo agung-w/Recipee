fdf3adfe610c1c4e8bf8574ce9ec9126e5035c4f
agung-w <suronokz3471@yahoo.com>
2023-03-15 21:01:00 +0700

    Add metric model

---
+++ lib/entities/metric.dart
@@ -0,0 +1,19 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'metric.g.dart';
+
+@JsonSerializable()
+class Metric {
+  final String id;
+  final String name;
+  final String abbrev;
+  const Metric({
+    required this.id,
+    required this.name,
+    required this.abbrev,
+  });
+
+  factory Metric.fromJson(Map<String, dynamic> json) => _$MetricFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MetricToJson(this);
+}
---
+++ lib/entities/metric.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'metric.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Metric _$MetricFromJson(Map<String, dynamic> json) => Metric(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      abbrev: json['abbrev'] as String,
+    );
+
+Map<String, dynamic> _$MetricToJson(Metric instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'abbrev': instance.abbrev,
+    };
