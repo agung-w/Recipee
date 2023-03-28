e37512dcb389adae81916142e47d0d24e57dce92
agung-w <suronokz3471@yahoo.com>
2023-03-28 22:24:09 +0700

    Add find tag services

--- lib/services/tag_services.dart
+++ lib/services/tag_services.dart
@@ -31,4 +31,35 @@ class TagServices {
       return const ApiResult.failed("Failed to get tag list. Try again later");
     }
   }
+
+  Future<ApiResult<Tag>> findTag({required String name}) async {
+    try {
+      Response result = await _dio.get(
+        "${dotenv.env['API_URL']}/tag/find?name=$name",
+        options: options ??
+            Options(headers: {
+              "Content-Type": "application/json",
+            }),
+      );
+      if (result.statusCode != 200) {
+        throw (DioError(
+            response: result, requestOptions: result.requestOptions));
+      }
+      Tag resultObj = Tag.fromJson(result.data['data']['tag']);
+      return ApiResult.success(resultObj);
+    } on DioError catch (e) {
+      String errorMessage = "Connection timeout";
+      if (e.response != null) {
+        if (e.response!.data['message'] is Map) {
+          var error = e.response!.data['message'];
+          var pos = error.keys.first.lastIndexOf('.');
+          errorMessage =
+              "${(pos != -1) ? error.keys.first.substring(pos + 1) : error.keys.first} ${error.values.first[0]}";
+        } else {
+          errorMessage = e.response!.data['message'];
+        }
+      }
+      return ApiResult.failed(errorMessage);
+    }
+  }
 }
